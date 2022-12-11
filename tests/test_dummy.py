43d171a1e8798907a986e5471e29960d813fde23
lazyoracle <contact@anuragsaharoy.me>
2022-12-11 16:32:34 -0500

    add more tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -4,3 +4,8 @@ def test_dummy():
     x = [1e-5, 1e-3, 1e-1]
     y = np.arccos(np.cos(x))
     np.testing.assert_allclose(x, y, rtol=1e-5, atol=0)
+
+def test_dummy_2():
+    x = [1e-5, 1e-3, 1e-1]
+    y = np.arccos(np.cos(x))
+    np.testing.assert_allclose(x, y, rtol=1e-5, atol=0)
