8362396f1693ce066c3e95dcf82f82b64bda5f9b
lazyoracle <contact@anuragsaharoy.me>
2022-12-11 17:55:12 -0500

    add tests

--- tests/test_muddy.py
+++ tests/test_muddy.py
@@ -4,3 +4,8 @@ def test_muddy():
     x = [1e-5, 1e-3, 1e-1]
     y = np.arccos(np.cos(x))
     np.testing.assert_allclose(x, y, rtol=1e-5, atol=0)
+
+def test_muddy_2():
+    x = [1e-5, 1e-3, 1e-1]
+    y = np.arccos(np.cos(x))
+    np.testing.assert_allclose(x, y, rtol=1e-5, atol=0)
